fcbc64563f8ece7e4b5efa59f96355d122b05b83
jgresham <johnsgresham@gmail.com>
2025-01-03 16:15:34 -0800

    simple connection to kv store and return value

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,50 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { AutoRouter, withParams } from 'itty-router'
+import { Redis } from '@upstash/redis'
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+
+export interface Env {
+	UPSTASH_TOKEN: string;
+  }
+
+const router = AutoRouter();
+
+router.get('/', () => new Response('Hello World!'));
+
+router.get('/wallet/:walletAddress/games', withParams, async (request: Request, env: Env, ctx: ExecutionContext) => {
+	const walletAddress = request.params.walletAddress;
+	console.log(walletAddress);
+	const redis = new Redis({
+		url: 'https://superb-marmoset-20181.upstash.io',
+		token: env.UPSTASH_TOKEN,
+	})
+	const games = await redis.hget('walletAddrToGames', walletAddress);
+  if(!games) {
+	return new Response('No games found');
+  } else {
+	return new Response(JSON.stringify(games));
+  }
+});
+
+
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+
+// 		// const redis = new Redis({
+// 		// 	url: 'https://superb-marmoset-20181.upstash.io',
+// 		// 	token: env.UPSTASH_TOKEN,
+// 		// })
+// 	  return router.handle(request, env, ctx);
+// 	}
+//   } satisfies ExportedHandler<Env>;
+
+export default { ...router } // this looks pointless, but trust us
+
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		// return router.handle(request, env, ctx);
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
